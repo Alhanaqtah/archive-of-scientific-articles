a0efd507f03dc3461ba5f0bf2530f48245432f3f
alexandr-fokin <sasafokin2@gmail.com>
2024-11-03 17:29:06 +0300

    feat: remove Search bar and second pagination

--- client/src/pages/Home/ui/Home.tsx
+++ client/src/pages/Home/ui/Home.tsx
@@ -1,7 +1,5 @@
 import { ArticlesTable } from "@/widgets/ArticlesTable";
 import { Pagination } from "@/widgets/Pagination";
-import { SearchBar } from "@/widgets/SearchBar";
-
 import styles from "./style.module.scss";
 import { Button } from "@/shared/ui/Button";
 import { Link } from "react-router-dom";
@@ -34,12 +32,6 @@ export function Home() {
 
       <main className={styles.homePage}>
         <h1>Все статьи</h1>
-        <div className={styles.toolBar}>
-          <div className={styles.searchBar}>
-            <SearchBar />
-          </div>
-          <Pagination page={page} onChange={handlePageChange} />
-        </div>
         <ArticlesTable articles={articles} />
         <div className={styles.pagination}>
           <Pagination page={page} onChange={handlePageChange} />
